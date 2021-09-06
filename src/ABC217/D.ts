35cbae59fa8b14555ed89c171af5c44a8f2fe1ff
Yamashita-Yoshimasa <yychbook040720@gmail.com>
2021-09-06 23:22:43 +0900

    ABC217

---
+++ src/ABC217/D.ts
@@ -0,0 +1,17 @@
+import { createInterface } from 'readline';
+
+// 入出力
+let lines: string[] = [];
+const reader = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.on('line', function (line: string) {
+  lines.push(line);
+});
+reader.on('close', function () {
+  Result(lines);
+});
+
+const Result = (input: string[]) => {};
